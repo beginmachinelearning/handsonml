fdac0c1f9f2d67454ceb65d339b8a8b6443a250f
maximdsouzaml <36703574+maximdsouzaml@users.noreply.github.com>
2018-03-26 14:25:57 +0530

    classification

---
+++ classfication.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import fetch_mldata
+mnist = fetch_mldata('MNIST original')
+mnist
+
+X, y = mnist["data"], mnist["target"]
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap = matplotlib.cm.binary,
+           interpolation="nearest")
+plt.axis("off")
+plt.show()
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+import numpy as np
+
+shuffle_index = np.random.permutation(60000)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+
+y_train_5 = (y_train == 5) 
+y_test_5 = (y_test == 5)
+
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([X_train[2]])
